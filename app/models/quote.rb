5ef1dfc85371dd1ab3d721d0921a8247ad1a8baf
John Ellison <john@john-ellison.com>
2015-02-02 23:11:46 +0000

    added unique_tag test, model and integrated into view

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,14 @@
 class Quote < ActiveRecord::Base
   validates :saying, :presence => true, :length => { :maximum => 150, :minimum => 12}
   validates :author, :presence => true, :length => { :maximum => 50, :minimum => 1 }
+
+  def unique_tag
+    abbr = self.author.split(" ").collect do |word|
+      word[0]
+  end
+
+  abbr.join + '#' + self.id.to_s
+
+  end
+
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'unique_tag' do
+    quote = Quote.create( author: 'Bob Stein', saying: 'I love science.')
+    puts quote.inspect
+
+    expected = 'BS#' + quote.id.to_s
+    actual = quote.unique_tag
+
+    assert_equal expected, actual
+  end
+
 end
